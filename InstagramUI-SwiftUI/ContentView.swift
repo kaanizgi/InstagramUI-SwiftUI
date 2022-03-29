add359574c7a6303d250df35689e92d7685762ef
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-29 23:19:50 +0300

    Initial Commit

---
+++ InstagramUI-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramUI-SwiftUI
+//
+//  Created by Kaan İzgi on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramUI-SwiftUI/InstagramUI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUI_SwiftUIApp.swift
+//  InstagramUI-SwiftUI
+//
+//  Created by Kaan İzgi on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
